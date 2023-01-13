cada58d4eeff2d3e933194afe46b45965f80ffa4
radespratama <puturades89@gmail.com>
2023-01-13 22:27:22 +0800

    add new resource and update adit task ⚡

--- app/Http/Controllers/Auth/Admin/AdminRegisterController.php
+++ app/Http/Controllers/Auth/Admin/AdminRegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth\Admin;
 use Exception;
 use App\Models\Admin;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\AdminResource;
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
 
@@ -19,25 +20,25 @@ class AdminRegisterController extends Controller
     public function __invoke(Request $request)
     {
         try {
-            $validated =  $this->validate($request, 
-            [
-                'username'=>'required|unique:admins|max:20',
-                'email'=>'required|email|unique:admins',
-                'password'=>'required|string|min:8|confirmed'
-            ]) ;
+            $validated = $this->validate(
+                $request,
+                [
+                    'username' => 'required|unique:admins|max:20',
+                    'email' => 'required|email|unique:admins',
+                    'password' => 'required|string|min:8|confirmed'
+                ]
+            );
 
             $validated['password'] = bcrypt($request->password);
             Admin::create($validated);
-            
+
             $admin = Admin::where('username', $request->username)->first();
             $token = $admin->createToken(env('APP_KEY'))->plainTextToken;
 
-            return response()->json([
-                'token_acces' => $token,
+            return (new AdminResource($admin))->additional([
                 'token_type' => 'Bearer',
-                'data' => $admin
-            ], 201);
-
+                'access_token' => $token,
+            ]);
         } catch (ValidationException $e) {
             return response()->json([
                 'message' => 'Validation Error',
